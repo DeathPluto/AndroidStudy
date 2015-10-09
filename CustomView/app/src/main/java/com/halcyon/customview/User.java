593dd89ea9907434bcb5b2096eb66c7544a125b2
LelouchsHalcyon <lh_halcyondays@163.com>
2015-10-09 15:44:47 +0800

    CustomView (cant work)

---
+++ CustomView/app/src/main/java/com/halcyon/customview/User.java
@@ -0,0 +1,19 @@
+package com.halcyon.customview;
+
+/**
+ * ©2015-2016 kcoin.inc.All Rights Reserved
+ *
+ * @author: L
+ * Description:
+ */
+public class User {
+    String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
